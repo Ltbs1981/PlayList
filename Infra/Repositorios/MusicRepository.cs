86ce53475e4bec8111fd30b939609c1cf82b48e5
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-26 23:04:51 -0300

    feito a classe music repository

---
+++ Infra/Repositorios/MusicRepository.cs
@@ -0,0 +1,34 @@
+﻿using PlayList.Core.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayList.Infra.Repositorios
+{
+    internal class MusicRepository
+    {
+        private static List<Musica> musicas = new List<Musica>();
+        private static int id = 0;
+
+        public void Add(Musica musica)
+        {
+            musica.Id = id++;
+            musicas.Add(musica);
+        }
+        public List<Musica> GetAll()
+        {
+            return musicas;
+        }
+        public void Update(Musica musica)
+        {
+            Musica banco = musicas.FirstOrDefault(i => i.Id == musica.Id);
+        }
+        public void Delete(Musica musica)
+        {
+            musicas.Remove(musica);
+        }
+    }
+}
