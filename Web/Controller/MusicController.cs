4cf954cbac3fb77280848790a3316c4da9e5261a
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-10-28 19:56:30 -0300

    feito o service

---
+++ Web/Controller/MusicController.cs
@@ -0,0 +1,33 @@
+﻿using PlayList.Core.Entidades;
+using PlayList.Servico;
+namespace PlayList.Web.Controller
+{
+    internal class MusicController
+           {
+        private MusicService _musicService;
+
+        public MusicController()
+        {
+            _musicService = new MusicService();
+        }
+        public void Criar(string nome, string artista, int duracao) 
+        {
+            Musica musica = new Musica()
+            {
+                Nome = nome,
+                Artista = artista,
+                Duracao = duracao
+            };
+            _musicService = new MusicService();
+            _musicService.Add(musica);
+        }
+        public void Ler()
+        {
+            var musicas = _musicService.GetAll();
+            foreach (var musica in musicas)
+            {
+                Console.WriteLine($"{musica.Id}{musica.Nome}{musica.Artista}{musica.Duracao}");
+            }
+        }
+    }
+}
