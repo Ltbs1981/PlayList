149173a94a1b350c7a52c5d4f46c07082dc5fd0c
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-11-04 03:05:16 -0300

    feito a classe filme

---
+++ Core/Entidades/Filme.cs
@@ -0,0 +1,19 @@
+﻿namespace PlayList.Core.Entidades
+{
+    internal class Filme : Midia
+    {
+        public string Diretor { get; set; }
+        public int DuracaoMinutos { get; set; }
+
+        public Filme(string nome, string diretor, int duracaoMinutos) : base(nome, diretor, duracaoMinutos)
+        {
+            Diretor = diretor;
+            DuracaoMinutos = duracaoMinutos;
+        }
+
+        public override void ExibirDetalhes()
+        {
+            Console.WriteLine($"Filme: {Nome}, Diretor: {Diretor}, Duração: {DuracaoMinutos} minutos");
+        }
+    }
+}
